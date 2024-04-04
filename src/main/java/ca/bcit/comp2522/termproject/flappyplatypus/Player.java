68890e98611abc0e551769d9550278bde97428d2
izcheung <icheung24@my.bcit.ca>
2024-04-04 14:57:49 -0700

    Add player class

---
+++ src/main/java/ca/bcit/comp2522/termproject/flappyplatypus/Player.java
@@ -0,0 +1,10 @@
+package ca.bcit.comp2522.termproject.flappyplatypus;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Player {
+    public static ImageView player() {
+       
+    }
+}
