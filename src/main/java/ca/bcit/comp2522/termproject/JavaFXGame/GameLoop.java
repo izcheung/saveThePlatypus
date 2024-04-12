3f8756bc5e0166b184ccdcff2e27f9b9d4cb7f0d
izcheung <icheung24@my.bcit.ca>
2024-04-11 22:39:16 -0700

    Change thread to timeline

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/GameLoop.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/GameLoop.java
@@ -1,30 +1,34 @@
 package ca.bcit.comp2522.termproject.JavaFXGame;
 
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.scene.Group;
+import javafx.util.Duration;
 
 public class GameLoop implements Runnable {
     private final Group root;
 
+    Enemy verticalEnemy = new VerticalEnemy();
+    Enemy horizontalEnemy = new HorizontalEnemy();
     public GameLoop(Group root) {
         this.root = root;
     }
 
     @Override
     public void run() {
-        while (!Run.gameOver) {
-            try {
-                Thread.sleep(2000); // Delay between cycles
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (!Run.gameOver) {
-                Platform.runLater(() -> {
-                    Enemy.generateEnemies(root);
-                });
-            }
-        }
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(3), event -> {
+                if (!Run.gameOver) {
+                    verticalEnemy.generateEnemies(root);
+                    horizontalEnemy.generateEnemies(root);
+                    Run.cyclesText.incrementCyclesCount();
+                    Run.levelUpText.updateLevel();
+                }
+            })
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
