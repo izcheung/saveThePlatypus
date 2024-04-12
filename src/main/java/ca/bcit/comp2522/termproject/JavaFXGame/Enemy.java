0c1aeca53ecb2a2cb1db21f25773e67d227b3138
izcheung <icheung24@my.bcit.ca>
2024-04-11 23:24:50 -0700

    Add javadoc to Enemy

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/Enemy.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/Enemy.java
@@ -9,15 +9,27 @@ import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 import java.util.Random;
 
+/**
+ * Represents an abstract class for enemy objects in the game.
+ */
 public abstract class Enemy implements Collision {
     private final colorEnum[] colorChoices = colorEnum.values();
     protected TranslateTransition translateTransition = new TranslateTransition();
     protected static final Random RANDOM = new Random();
 
+    /**
+     * An enumeration of all available color options for enemies.
+     */
     public enum colorEnum {
         TEAL, ORANGE, LIME, RED
     }
 
+    /**
+     * Moves the enemy object.
+     *
+     * @param root   The root group of the game scene.
+     * @param circle The circle representing the enemy object.
+     */
     public void move(Group root, Circle circle) {
         translateTransition.setDuration(Duration.millis(Run.speed));
         translateTransition.setNode(circle);
@@ -29,6 +41,14 @@ public abstract class Enemy implements Collision {
         root.getChildren().add(circle);
         translateTransition.play();
     }
+
+    /**
+     * Checks for collision between the player and the enemy.
+     *
+     * @param player The image view representing the player.
+     * @param enemy  The circle representing the enemy.
+     * @param root   The root group of the game scene.
+     */
     public void checkCollision(ImageView player, Circle enemy, Group root) {
         boolean collisionDetected = player.getBoundsInParent().intersects(enemy.getBoundsInParent());
         if (collisionDetected) {
@@ -51,13 +71,30 @@ public abstract class Enemy implements Collision {
         };
     }
 
-    void generateEnemies(Group root) {
+    /**
+     * Generates enemies within the game world.
+     *
+     * @param root The root group of the game scene.
+     */
+    protected void generateEnemies(Group root) {
         Circle circle = createCircle();
         Color randomColor = convertColorEnumToJavaFX(generateRandomColor());
         circle.setFill(randomColor);
         move(root, circle);
     }
+
+    /**
+     * Abstract method to create a circle representing the enemy.
+     *
+     * @return The circle representing the enemy.
+     */
     abstract Circle createCircle();
 
+    /**
+     * Abstract method to set the specific movement direction of the enemy.
+     *
+     * @param root   The root group of the game scene.
+     * @param circle The circle representing the enemy.
+     */
     abstract void setSpecificMovementDirection(Group root, Circle circle);
 }
