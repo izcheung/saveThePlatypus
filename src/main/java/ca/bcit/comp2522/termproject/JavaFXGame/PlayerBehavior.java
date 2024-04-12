af241d51dc51963c265ca6c8493d589d9c8f0df5
izcheung <icheung24@my.bcit.ca>
2024-04-11 18:04:08 -0700

    Add PlayerBehavior interface

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/PlayerBehavior.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/PlayerBehavior.java
@@ -3,5 +3,6 @@ package ca.bcit.comp2522.termproject.JavaFXGame;
 import javafx.scene.input.KeyEvent;
 
 public interface PlayerBehavior {
-    void processKeyPress(KeyEvent event);
+    void processKeyPress(final KeyEvent event);
+
 }
