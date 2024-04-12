2941c8fa12b1ff735ebc6eaef263b92057c3a81f
izcheung <icheung24@my.bcit.ca>
2024-04-11 23:22:20 -0700

    Add javadoc to CycleText

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/CycleText.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/CycleText.java
@@ -3,13 +3,26 @@ package ca.bcit.comp2522.termproject.JavaFXGame;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
 
+/**
+ * Represents a text node that displays the number of cycles.
+ */
 public class CycleText extends Text {
-    protected CycleText(int x, int y) {
+
+    /**
+     * Constructs a CycleText object with the specified position.
+     *
+     * @param x The x-coordinate of the text position.
+     * @param y The y-coordinate of the text position.
+     */
+    public CycleText(int x, int y) {
         super(x, y, "Cycles: 0");
         setText("Cycles: " + Run.cyclesCount);
         setFill(Color.WHITE);
     }
 
+    /**
+     * Increments the cycles count and updates the text accordingly.
+     */
     protected void incrementCyclesCount() {
         Run.cyclesCount++;
         setText("Cycles: " + Run.cyclesCount);
