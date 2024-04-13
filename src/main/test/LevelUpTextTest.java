1753aa2393f5c69957b94dd0a2e631a0591da7f6
izcheung <icheung24@my.bcit.ca>
2024-04-12 23:17:02 -0700

    Add unit test for LevelUpText

---
+++ src/main/test/LevelUpTextTest.java
@@ -0,0 +1,15 @@
+import ca.bcit.comp2522.termproject.JavaFXGame.LevelUpText;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class LevelUpTextTest {
+    @Test
+    public void testLevelUpText() {
+        LevelUpText levelUpText = new LevelUpText(1, 2);
+        String expectedInitialText = "Level: 0";
+        assertEquals(expectedInitialText, (levelUpText.getText()));
+    }
+
+
+}
