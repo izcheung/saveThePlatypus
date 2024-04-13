96cfcef61ba7f1c3b0ac014955d698cb675d5c01
izcheung <icheung24@my.bcit.ca>
2024-04-12 23:17:15 -0700

    Add javaDocs

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/LevelUpText.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/LevelUpText.java
@@ -3,14 +3,29 @@ package ca.bcit.comp2522.termproject.JavaFXGame;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
 
-
+/**
+ * Represents a text node that displays the current level of the player.
+ *
+ * @author Irene Cheung
+ * @version 2024
+ */
 public class LevelUpText extends Text {
+
+    /**
+     * Constructs a LevelUpText object with the specified position.
+     *
+     * @param x The x-coordinate of the text position.
+     * @param y The y-coordinate of the text position.
+     */
     public LevelUpText(int x, int y) {
         super(x, y, "Level: 0");
         setText("Level: " + Player.level);
         setFill(Color.WHITE);
     }
 
+    /**
+     * Updates the level text based on the number of cycles completed.
+     */
     protected void updateLevel() {
         if (Run.cyclesCount % 5 == 0) {
             Player.level++;
