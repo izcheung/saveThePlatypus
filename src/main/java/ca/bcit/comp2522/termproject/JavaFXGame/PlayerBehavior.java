0a4917f2127d913d6786d8a929c01341f6e7452c
izcheung <icheung24@my.bcit.ca>
2024-04-12 22:58:52 -0700

    Add javadoc for class and method

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/PlayerBehavior.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/PlayerBehavior.java
@@ -2,7 +2,19 @@ package ca.bcit.comp2522.termproject.JavaFXGame;
 
 import javafx.scene.input.KeyEvent;
 
-public interface PlayerBehavior {
-    void processKeyPress(final KeyEvent event);
 
+/**
+ * Defines the behavior for a player character in the game.
+ *
+ * @author Irene Cheung
+ * @version 2024
+ */
+public interface PlayerBehavior {
+
+    /**
+     * Processes the key press event to control the player character.
+     *
+     * @param event The key event representing the key press.
+     */
+    void processKeyPress(final KeyEvent event);
 }
