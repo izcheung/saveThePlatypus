28c623e454c89b302e3fe4be59e84eb91f93bc52
izcheung <icheung24@my.bcit.ca>
2024-04-12 22:56:55 -0700

    Add javadoc

--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/HorizontalEnemy.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/HorizontalEnemy.java
@@ -1,14 +1,39 @@
 package ca.bcit.comp2522.termproject.JavaFXGame;
 
+import javafx.animation.TranslateTransition;
 import javafx.scene.Group;
 import javafx.scene.shape.Circle;
 
+import java.util.Random;
+
+/**
+ * Represents an enemy that moves horizontally across the game screen.
+ *
+ * @author Irene Cheung
+ * @version 2024
+ */
 public class HorizontalEnemy extends Enemy {
+
+    /**
+     * Sets the specific movement direction for the horizontal enemy.
+     *
+     * @param root   The root group of the game scene.
+     * @param circle The circle representing the enemy.
+     */
     protected void setSpecificMovementDirection(Group root, Circle circle) {
+        TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setByY(Run.APP_HEIGHT + 50);
     }
+
+    /**
+     * Creates a circle representing the horizontal enemy.
+     *
+     * @return The circle representing the horizontal enemy.
+     */
     @Override
     public Circle createCircle() {
+        Random RANDOM = new Random();
         return new Circle(RANDOM.nextInt(Run.APP_WIDTH), -25, 25);
     }
+
 }
--- src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/VerticalEnemy.java
+++ src/main/java/ca/bcit/comp2522/termproject/JavaFXGame/VerticalEnemy.java
@@ -1,14 +1,37 @@
 package ca.bcit.comp2522.termproject.JavaFXGame;
 
+import javafx.animation.TranslateTransition;
 import javafx.scene.Group;
 import javafx.scene.shape.Circle;
 
+import java.util.Random;
+
+/**
+ * Represents an enemy that moves vertically across the screen.
+ * @author Irene Cheung
+ * @version 2024
+ */
 public class VerticalEnemy extends Enemy {
-    protected void setSpecificMovementDirection(Group root, Circle circle) {
+
+    /**
+     * Sets the specific movement direction for the vertical enemy.
+     *
+     * @param root   The root group of the game scene.
+     * @param circle The circle representing the enemy.
+     */
+    public void setSpecificMovementDirection(Group root, Circle circle) {
+        TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setByX(Run.APP_WIDTH + 50);
     }
+
+    /**
+     * Creates a circle representing the vertical enemy.
+     *
+     * @return The circle representing the vertical enemy.
+     */
     @Override
     public Circle createCircle() {
+        Random RANDOM = new Random();
         return new Circle(-25, RANDOM.nextInt(Run.APP_HEIGHT), 25);
     }
 }
